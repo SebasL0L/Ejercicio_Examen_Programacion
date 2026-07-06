21b708a0b8a8a26f2a019d5502f521aae254a082
unknown <se.carrascol@duocuc.cl>
2026-07-05 23:28:19 -0400

    Agregar menu principal y datos iniciales

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
 # Archivo principal del proyecto
+import modulo
+
+productos = {
+    "P101":["Cuaderno","Papelería",2490,True],
+    "P102":["Lápiz","Papelería",590,True],
+    "P103":["Botella","Accesorios",6990,False],
+    "P104":["Mochila","Accesorios",24990,True]
+}
+
+inventario = {
+    "P101":[30,15],
+    "P102":[120,50],
+    "P103":[0,10],
+    "P104":[8,25]
+}
+
+def main():
+
+    while True:
+
+        print("========= MENÚ =========")
+        print("1. Stock por categoría")
+        print("2. Buscar precio")
+        print("3. Actualizar precio")
+        print("4. Agregar producto")
+        print("5. Eliminar producto")
+        print("6. Mostrar productos")
+        print("7. Salir")
+
+        opcion = modulo.leer_opcion()
+
+        if opcion == 7:
+            break
+
+main()
--- modulo.py
+++ modulo.py
@@ -1,1 +1,18 @@
 # Funciones del sistema
+
+def leer_opcion():
+
+    while True:
+
+        try:
+
+            opcion = int(input("Seleccione una opción: "))
+
+            if 1 <= opcion <= 7:
+                return opcion
+
+            print("Debe seleccionar una opción válida.")
+
+        except ValueError:
+
+            print("Debe ingresar un número.")
