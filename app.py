ab3ec1432116dcde084b300c5bc0cc3dfd568ce2
unknown <se.carrascol@duocuc.cl>
2026-07-05 23:44:08 -0400

    Integrar menu principal con funciones del modulo usando match-case

--- app.py
+++ app.py
@@ -1,36 +1,65 @@
+import os
+
 # Archivo principal del proyecto
-import modulo
+import modulo as mo
 
-productos = {
-    "P101":["Cuaderno","Papelería",2490,True],
-    "P102":["Lápiz","Papelería",590,True],
-    "P103":["Botella","Accesorios",6990,False],
-    "P104":["Mochila","Accesorios",24990,True]
-}
-
-inventario = {
-    "P101":[30,15],
-    "P102":[120,50],
-    "P103":[0,10],
-    "P104":[8,25]
-}
 
 def main():
 
+    productos = {
+        "P101": ["Cuaderno", "Papelería", 2490, True],
+        "P102": ["Lápiz", "Papelería", 590, True],
+        "P103": ["Botella", "Accesorios", 6990, False],
+        "P104": ["Mochila", "Accesorios", 24990, True]
+    }
+
+    inventario = {
+        "P101": [30, 15],
+        "P102": [120, 50],
+        "P103": [0, 10],
+        "P104": [8, 25]
+    }
+
     while True:
 
-        print("========= MENÚ =========")
+        print("\n========= MENÚ PRINCIPAL =========")
         print("1. Stock por categoría")
-        print("2. Buscar precio")
+        print("2. Buscar productos por rango de precio")
         print("3. Actualizar precio")
         print("4. Agregar producto")
         print("5. Eliminar producto")
         print("6. Mostrar productos")
         print("7. Salir")
 
-        opcion = modulo.leer_opcion()
+        opcion = mo.leer_opcion()
 
-        if opcion == 7:
-            break
+        match opcion:
 
-main()
+            case 1:
+                categoria = input("Ingrese categoría: ")
+                mo.stock_categoria(categoria, productos, inventario)
+
+            case 2:
+                precio_min = int(input("Precio mínimo: "))
+                precio_max = int(input("Precio máximo: "))
+                mo.buscar_precio(precio_min, precio_max, productos, inventario)
+
+            case 3:
+                codigo = input("Código: ")
+                nuevo_precio = int(input("Nuevo precio: "))
+
+                if mo.actualizar_precio(codigo, nuevo_precio, productos):
+                    print("Precio actualizado correctamente.")
+                else:
+                    print("Código inexistente.")
+
+            case 6:
+                mo.mostrar_productos(productos, inventario)
+
+            case 7:
+                print("Programa finalizado.")
+                break
+
+
+if __name__ == "__main__":
+    main()
