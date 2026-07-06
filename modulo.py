9c927ff9c9f453124981f7f817c7417aac364870
unknown <se.carrascol@duocuc.cl>
2026-07-05 23:30:25 -0400

    Implementar funciones de validacion y lectura del menu

--- modulo.py
+++ modulo.py
@@ -16,3 +16,51 @@ def leer_opcion():
         except ValueError:
 
             print("Debe ingresar un número.")
+
+
+
+
+# valicaiones 
+def validar_codigo(codigo, productos):
+
+    codigo = codigo.strip().upper()
+
+    if codigo == "":
+        return False
+
+    if codigo in productos:
+        return False
+
+    return True
+
+def validar_nombre(nombre):
+
+    return nombre.strip() != ""
+
+
+def validar_categoria(categoria):
+
+    return categoria.strip() != ""
+
+
+def validar_precio(precio):
+
+    return precio > 0
+
+
+def validar_disponible(disponible):
+
+    disponible = disponible.strip().lower()
+
+    return disponible in ["s", "n"]
+
+
+def validar_stock(stock):
+
+    return stock >= 0
+
+
+def validar_vendidos(vendidos):
+
+    return vendidos >= 0
+
